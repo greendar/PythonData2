4f202677097589590ec688b3ee907fdc24483790
Mr Green <dwgreen@hwdsb.on.ca>
2020-06-25 08:21:05 -0400

    inserted comment of plan for next steps

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ areas = ['British Columbia', 'Alberta', 'Saskatchewan', 'Manitoba',
 'Ontario', 'Quebec', 'Nova Scotia', 'New Brunswick', 'Newfoundland and Labrador',
 'Prince Edward Island', 'Yukon', 'Nunavut', 'Northwest Territories', 'Canada']
 
+#########################################
+'''
+Next make it so that if a command line choice
+is not in areas then will default to internal
+menu
+'''
+#########################################
 
 loadCanadaData()
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 1: # can use command line
     choice = sys.argv[1]
-else:
+else: # or the internal menu
     choice = areaMenu(areas)
 
 area = Region(choice + ".csv")
